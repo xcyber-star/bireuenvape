6daf38402d7428d8e985dc9bc0d8a2322485bfd5
Xcyber_roy <64800088+xcyber-star@users.noreply.github.com>
2024-12-20 11:29:23 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('Terima kasih sudah membeli produk kami! Kami akan segera menghubungi Anda.');
+    });
+});
